bf6d6cac111840804c62d6ea2961e4b5e1efc6c3
RabieImghi <rabieaitimghi7@gmail.com>
2024-10-28 20:47:25 +0100

    first version of project

---
+++ src/main/java/org/rabie/hunters_league/HuntersLeagueApplication.java
@@ -0,0 +1,14 @@
+package org.rabie.hunters_league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class HuntersLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuntersLeagueApplication.class, args);
+    }
+
+}
