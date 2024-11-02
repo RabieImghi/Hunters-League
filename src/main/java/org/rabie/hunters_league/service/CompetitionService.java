e7852ce386138bd1bf3fdadcd81ee95720ddad7c
RabieImghi <rabieaitimghi7@gmail.com>
2024-11-02 21:58:52 +0100

    create function for get detail of competition and get all competetion

--- src/main/java/org/rabie/hunters_league/service/CompetitionService.java
+++ src/main/java/org/rabie/hunters_league/service/CompetitionService.java
@@ -3,9 +3,12 @@ package org.rabie.hunters_league.service;
 import org.rabie.hunters_league.domain.Competition;
 import org.rabie.hunters_league.exceptions.CompetitionException;
 import org.rabie.hunters_league.repository.CompetitionRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -26,5 +29,9 @@ public class CompetitionService {
         if(lastCompetition!=null && lastCompetition.getDate().isAfter(competition.getDate().minusDays(7)))
             throw new CompetitionException("You can just create a competition every week");
         return competitionRepository.save(competition);
+    }
+    public Page<Competition> findAll(int page, int size){
+        PageRequest pageRequest = PageRequest.of(page,size);
+        return competitionRepository.findAll(pageRequest);
     }
 }
--- src/main/java/org/rabie/hunters_league/web/api/CompetitionController.java
+++ src/main/java/org/rabie/hunters_league/web/api/CompetitionController.java
@@ -6,11 +6,13 @@ import org.rabie.hunters_league.service.CompetitionService;
 import org.rabie.hunters_league.web.vm.mapper.CompetitionMapper;
 import org.rabie.hunters_league.web.vm.request.CompetitionVm;
 import org.rabie.hunters_league.web.vm.response.CompetitionResponseVm;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/competition")
@@ -31,4 +33,15 @@ public class CompetitionController {
         return ResponseEntity.ok(competitionMapper.toCompetitionResponseVm(savedCompetition));
     }
 
+    @GetMapping("/details")
+    public Page<CompetitionResponseVm> getCompetition(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "50") int size ){
+        Page<Competition> competitionList = competitionService.findAll(page, size);
+        return competitionList.map(competitionMapper::toCompetitionResponseVm);
+    }
+    @GetMapping("/details/{id}")
+    public List<CompetitionResponseVm> getCompetitionById(@PathVariable UUID id){
+        Competition competition = competitionService.getById(id);
+        CompetitionResponseVm competitionResponseVm = competitionMapper.toCompetitionResponseVm(competition);
+        return List.of(competitionResponseVm);
+    }
 }
